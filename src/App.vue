0a950f2f19103635903379507bdfe90e2db6a3f2
Durgesh002 <durgeshjadhav1018@gmail.com>
2023-10-05 18:56:45 +0530

    form-handling-Tutorials

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col">
+        <p class="h3 text-success">App Component</p>
+        <p class="fw-italic">Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia ipsa eligendi natus pariatur eaque sit ullam, error earum cupiditate maiores, nihil distinctio veritatis blanditiis! Aperiam quam quisquam reprehenderit corporis accusantium.</p>
+      </div>
+    </div>
+
+    <Register/>
+
+  </div>
+</template>
+
+
+<script>
+import Register from './components/Register.vue';
+
+export default{
+  name: 'App',
+  components: {
+    Register,
+  },
+  data: function(){
+    return{
+
+    }
+  }
+}
+</script>
+
+
+<style></style>
---
+++ src/components/Register.vue
@@ -0,0 +1,53 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-3">
+                <div class="card shadow-lg">
+                    <div class="card-header">
+                        <p class="h3">Register Here!!</p>
+                    </div>
+                    <div class="card-body bg-light">
+                        <form @submit.prevent="register">
+                            <div class="mb-2">
+                                <input type="text" class="form-control" placeholder="Name goes here..." v-model="user.name">
+                            </div>
+                            <div class="mb-2">
+                                <input type="email" class="form-control" placeholder="e.g. example@gmail.com" v-model="user.email">
+                            </div>
+                            <div class="mb-2">
+                                <input type="password" class="form-control" placeholder="Password goes here..." v-model="user.password">
+                            </div>
+                            <div class="mb-2">
+                                <input class="btn btn-success" type="submit" value="Register">
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Register',
+        data: function(){
+            return{
+                user: {
+                    name: '',
+                    email: '',
+                    password: '',
+                }
+            }
+        },
+        methods: {
+            register: function(){
+                console.log(this.user)
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
